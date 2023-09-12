2f133b92b175e898804ddc095432ad31d97ade64
Pampyz <erikrydene@gmail.com>
2023-09-12 22:08:56 +0200

    feat: add beginning template of main file

---
+++ main.py
@@ -0,0 +1,28 @@
+import os
+import sys
+
+class Asset:
+    def __init__(self, global_supply):
+        pass
+
+    def create(self, n):
+        pass
+
+class InflationaryAsset(Asset):
+    def __init__(self, global_supply, inflation_rate):
+        pass
+
+class Actor:
+    def __init__(self, policy):
+        pass
+
+class RandomActor(Actor):
+    def __init__(self):
+        pass
+
+
+def main():
+    pass
+
+if __name__=='__main__':
+    main()
